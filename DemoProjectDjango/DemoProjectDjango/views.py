f117340e701f4d7f07cec9a2e490f8349b8af767
Mohit Rikhi <76819254+mohit11R@users.noreply.github.com>
2025-01-19 22:20:01 +0530

    Delete DemoProjectDjango directory

--- DemoProjectDjango/DemoProjectDjango/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def home(request):
-    # return HttpResponse("Welcome to Mohit World!")
-    return render(request,"index.html")
-
-def about(request):
-    return HttpResponse("Welcome to Mohit World!: Building Websites")
--- DemoProjectDjango/chai/views.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-from django.shortcuts import render
-
-def all_chai(request):
-    print("Template dirs:", settings.TEMPLATES[0]['DIRS'])
-    return render(request, 'chai/all_chai.html')
