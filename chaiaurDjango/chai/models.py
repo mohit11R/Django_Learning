5bbd3a81dc5b8f0445fc7c784f97628f142f13c4
=Mohit11R <rikhimohit88@gmail.com>
2025-02-11 21:32:49 +0530

    update

--- chaiaurDjango/chai/models.py
+++ chaiaurDjango/chai/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class ChaiVarity(models.Model):
+    CHAI_TYPE_CHOICES = [
+        ('ML','MASALA'),
+        ('GR', 'GINGER'),
+        ('KL','KIWI'),
+        ('PL', 'PLAIN')
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = 'chais/')
+    date_added = models.DateTimeField(default=timezone.now)
+    types = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICES)
+
+
+    def __str__(self):
+        return self.name
--- chaiaurDjango/chai/views.py
+++ chaiaurDjango/chai/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import ChaiVarity
 # Create your views here.
 def all_chai(request):
-    return render(request, 'all_chai.html')
+    chais = ChaiVarity.objects.all()
+    return render(request, 'all_chai.html' , {'chais':chais})
